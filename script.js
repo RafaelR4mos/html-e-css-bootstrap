193774d24da77fb14b7f928498c9aa25e4f8e2a6
Rafael <rafaelramos0401@gmail.com>
2023-05-25 14:17:11 -0300

    feat: finalizada pagina de avaliacao

---
+++ script.js
@@ -0,0 +1,24 @@
+let alertTrigger = false;
+const toastLiveExample = document.getElementById('liveToast')
+const form = document.querySelector('.needs-validation')
+
+  form.addEventListener('submit', event => {
+    
+    event.preventDefault()
+    if (!form.checkValidity()) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    form.classList.add('was-validated')
+
+    if(form.checkValidity()) {
+      const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+      toastBootstrap.show()
+      form.reset();
+      form.classList.remove('was-validated')
+    }
+
+  }, false)
+
+
